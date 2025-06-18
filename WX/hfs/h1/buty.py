8ee8836bc4601bf148647880e85f09167f777a31
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-06-18 12:29:41 +0000

    🫦

---
+++ WX/hfs/h1/buty.py
@@ -0,0 +1,21 @@
+# /////////////////////////////////////////////////////////////////////
+# h1 - SmolAgents actua course from docs
+# /////////////////////////////////////////////////////////////////////
+
+# --- Imports ---
+
+from src.t1 import t1_main
+from src.utz import eline, tline
+
+# --- App Code ---
+
+
+def buty():
+    t1_main()
+    # t1_tez_main()
+
+
+if __name__ == "__main__":
+    tline()
+    buty()
+    eline()
