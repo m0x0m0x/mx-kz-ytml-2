4ffe69d2575873777bd6e522d887f5bced9c2843
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-06-19 16:48:54 +0000

    🫦

--- WX/hfs/h1/runz.sh
+++ WX/hfs/h1/runz.sh
@@ -15,20 +15,34 @@ export NC='\033[0m' # No Color
 # Commands
 
 hea1() {
-    echo -e "${CYAN}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~${NC}"
-    echo -e "${PURPLE}$1${NC}"
-    echo -e "${CYAN}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~${NC}"
+  echo -e "${CYAN}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~${NC}"
+  echo -e "${PURPLE}$1${NC}"
+  echo -e "${CYAN}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~${NC}"
 }
 
 # Execution Zone
 ru1() {
-    hea1 "Execute UV "
-    co1="uv run buty.py"
-    echo -e "${GREEN} Executing... "
-    echo -e " ${co1} ${NC}"
-    eval "${co1}"
+  hea1 "Execute UV "
+  co1="uv run buty.py"
+  echo -e "${GREEN} Executing... "
+  echo -e " ${co1} ${NC}"
+  eval "${co1}"
+}
+
+# DockerRun
+dockrun_once() {
+  IMAGE_NAME="my-app"
+
+  echo "🛠  Building image..."
+  docker build -t "$IMAGE_NAME" . || return 1
+
+  echo "🚀 Running container..."
+  docker run --rm "$IMAGE_NAME"
+
+  echo "🧹 Removing image..."
+  docker rmi "$IMAGE_NAME" >/dev/null || echo "⚠️ Failed to remove image"
 }
 
 # Exection
-
 ru1
+# dockrun_once
