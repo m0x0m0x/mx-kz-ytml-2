3a85f7d4663097a1bb83edf7117ed2cc4b26495f
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-06-20 14:26:38 +0000

    🫦

--- WX/vez/aie-feb-25-starter/src/index.ts
+++ WX/vez/aie-feb-25-starter/src/index.ts
@@ -1,14 +1,3 @@
-import { groq } from "@ai-sdk/groq"
-import { generateText } from "ai"
 import "dotenv/config"
 
-async function main() {
-  const { text } = await generateText({
-    model: groq("gemma2-9b-it"),
-    prompt: "Write a vegetarian lasagna recipe for 4 people.",
-  })
-
-  console.log(text)
-}
-
-main()
+console.log("Hello, world!")
