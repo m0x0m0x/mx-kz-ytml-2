f3490f3de8ab90cae2115bec0be7d079396dfdef
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-06-21 12:57:24 +0000

    🫦

--- WX/vez/aie-feb-25-starter/src/tu1.ts
+++ WX/vez/aie-feb-25-starter/src/tu1.ts
@@ -4,18 +4,20 @@ tu1.ts - These are all the functions being written from the tutorial
 
 // --- Imports Zone ---
 
+import boxen from "boxen"
 import chalk from "chalk"
 
 // --- Main Function Call ---
 
 export async function t1_main() {
-  console.log(chalk.bold.blue("Tu1 Main Function Call"))
-  const boxedMessage = boxen(result.text, {
+  const msg1 = "Tu1 - Main Tutorial Function Calls"
+  const boxedMessage = boxen(msg1, {
     padding: 1,
     margin: 1,
     borderStyle: "round",
     borderColor: "yellow",
   })
+  console.log(chalk.bold.blue(boxedMessage))
 }
 
 // --- Sub Function called
