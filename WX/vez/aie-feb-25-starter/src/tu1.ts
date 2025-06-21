014be5ac8e75727de5451e62210e01df7e3db9f6
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-06-21 13:16:01 +0000

    🫦

--- WX/vez/aie-feb-25-starter/src/tu1.ts
+++ WX/vez/aie-feb-25-starter/src/tu1.ts
@@ -4,6 +4,9 @@ tu1.ts - These are all the functions being written from the tutorial
 
 // --- Imports Zone ---
 
+import { groq } from "@ai-sdk/groq"
+import { generateText } from "ai"
+import "dotenv/config"
 import { printTutorialHeader } from "./wm"
 
 // --- Main Function Call ---
@@ -13,3 +16,17 @@ export async function t1_main() {
 }
 
 // --- Sub Function called
+
+export async function t1_func1() {
+  printTutorialHeader("t1_func1", "t1_func1")
+
+  const result = await generateText({
+    model: groq("llama-3.3-70b-versatile"),
+    messages: [
+      {
+        role: "user",
+        content: "When was the AI engineer summit 2025",
+      },
+    ],
+  })
+}
