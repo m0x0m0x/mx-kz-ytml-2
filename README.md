97e28a37872e3e9dfccba8c5f1cbd22b19c01fa1
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-06-21 12:48:56 +0000

    🫦

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
+1. [mx-kz-ytml-2](#mx-kz-ytml-2)
+
 # mx-kz-ytml-2
+
 Conti - https://github.com/kachraz/kz-ytml-1
+
+> This is a continuation of the work for SmolAgens and with some other learnings
--- WX/vez/aie-feb-25-starter/src/index.ts
+++ WX/vez/aie-feb-25-starter/src/index.ts
@@ -5,43 +5,8 @@ https://github.com/nicoalbanese/aie-feb-25-starter
 
 // --- Imports Zone ---
 
-import { groq } from "@ai-sdk/groq"
-import { generateText } from "ai"
-import boxen from "boxen"
-import chalk from "chalk"
 import "dotenv/config"
-import { googleSearchGrounding } from "./me1"
+import { main1 } from "./me2"
 
 // --- Function Definitions ---
-
-// This function is following the tutorial
-const main = async () => {
-  const result = await generateText({
-    model: groq("llama-3.3-70b-versatile"),
-    messages: [
-      {
-        role: "user",
-        content: "When was the AI engineer summit 2025",
-      },
-    ],
-  })
-
-  // Using boxen for boxes and chalk for color output
-  console.log(chalk.bold.blue("Generated Recipe:"))
-  const boxedMessage = boxen(result.text, {
-    padding: 1,
-    margin: 1,
-    borderStyle: "round",
-    borderColor: "yellow", // Optional background color
-  })
-  console.log(chalk.greenBright(boxedMessage))
-  console.log(chalk.bold.green("✔ Operation completed"))
-}
-
-// --- Function Execution ---
-
-// My function
-// m1_main()
-// main()
-// compoundBetaTest()
-googleSearchGrounding()
+main1()
