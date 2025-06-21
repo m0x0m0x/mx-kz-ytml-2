a6e17e6f5703909633b6eef13f8d4584087b5b85
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-06-21 09:01:45 +0000

    🫦

--- WX/vez/aie-feb-25-starter/src/index.ts
+++ WX/vez/aie-feb-25-starter/src/index.ts
@@ -3,6 +3,7 @@ import { generateText } from "ai"
 import boxen from "boxen"
 import chalk from "chalk"
 import "dotenv/config"
+import { compoundBetaTest } from "./me1"
 
 const main = async () => {
   const result = await generateText({
@@ -28,4 +29,5 @@ const main = async () => {
 
 // My function
 // m1_main()
-main()
+// main()
+compoundBetaTest()
--- WX/vez/aie-feb-25-starter/src/me1.ts
+++ WX/vez/aie-feb-25-starter/src/me1.ts
@@ -8,11 +8,44 @@ export async function m1_main() {
   explainAtmosphereInGangstaRap()
 }
 
+// Query closed model from groq
 export async function explainAtmosphereInGangstaRap() {
   try {
     const result = await generateText({
       model: groq("llama-3.3-70b-versatile"),
       prompt: "Explain what is atmosphere, in gangsta rap style",
+    })
+
+    console.log(chalk.bold.blue("🔥 Atmosphere, Gangsta Style:"))
+
+    const boxedMessage = boxen(result.text, {
+      padding: 1,
+      margin: 1,
+      borderStyle: "round",
+      borderColor: "yellow",
+    })
+
+    console.log(chalk.greenBright(boxedMessage))
+    console.log(chalk.bold.green("✔ Operation completed"))
+
+    return result.text // Optional: return the response for reuse
+  } catch (error) {
+    console.error(chalk.red.bold("💥 Error:"), error.message)
+    throw error
+  }
+}
+
+// Query Compount Beta which also has web search
+export async function compoundBetaTest() {
+  try {
+    const result = await generateText({
+      model: groq("Compound Beta"),
+      messages: [
+        {
+          role: "user",
+          content: "Explain the economic outlook in 2025",
+        },
+      ],
     })
 
     console.log(chalk.bold.blue("🔥 Atmosphere, Gangsta Style:"))
