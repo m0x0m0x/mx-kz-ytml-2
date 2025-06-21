e867efff80a90be70db417f682b4bc07180b8dbd
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-06-21 14:51:30 +0000

    🫦

--- WX/vez/aie-feb-25-starter/src/tu1.ts
+++ WX/vez/aie-feb-25-starter/src/tu1.ts
@@ -110,12 +110,7 @@ async function t1_func3() {
 
   const result = await generateText({
     model: groq("llama-3.3-70b-versatile"),
-    messages: [
-      {
-        role: "user",
-        content: "When was the AI engineer summit 2025",
-      },
-    ],
+    prompt: "What is 1000 + 2000",
     tools: {
       addNumbers: tool({
         description: "Add two numbers",
