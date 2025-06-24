41b6d7f0f84aece167e8ceedaf0939f6847d8bda
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-06-24 09:57:05 +0000

    🫦

--- WX/hfs/h2/src/gh1.py
+++ WX/hfs/h2/src/gh1.py
@@ -4,9 +4,26 @@
 
 # --- Imports Zone ---
 
+import os
+
+from dotenv import load_dotenv
+from rich import print as rpr
+
+from .utz import he1
 
 # --- Vars ---
+load_dotenv("src/.azz")
+gh_t = os.getenv("GHB")
 
 # --- Main Function ---
 
+
+def gh1_main():
+    fn1()
+
 # --- Sub Function ---
+
+
+def fn1():
+    he1("Function 1")
+    rpr(f"[green] GHB: {gh_t} [/green]")
